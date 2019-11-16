b4f5355279ac827c07424f11bbddc1a7ba691f4c
Ben Rimmasch <ben@regoconsulting.com>
2019-11-15 21:50:53 -0700

    + import url

--- src/drivers/ring-virtual-keypad.groovy
+++ src/drivers/ring-virtual-keypad.groovy
@@ -15,13 +15,15 @@
  *
  *  Change Log:
  *  2019-04-26: Initial
+ *  2019-11-15: Import URL
  *
  */
 
 import groovy.json.JsonSlurper
 
 metadata {
-  definition(name: "Ring Virtual Keypad", namespace: "codahq-hubitat", author: "Ben Rimmasch") {
+  definition(name: "Ring Virtual Keypad", namespace: "codahq-hubitat", author: "Ben Rimmasch",
+    importUrl: "https://raw.githubusercontent.com/codahq/ring_hubitat_codahq/master/src/drivers/ring-virtual-keypad.groovy") {
     capability "Sensor"
     capability "Motion Sensor"
     capability "Audio Volume"
