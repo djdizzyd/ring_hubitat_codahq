b9a49858b06a2fff91fa13290058581e97c6bea9
Ben Rimmasch <ben@regoconsulting.com>
2019-11-15 21:33:15 -0700

    + import url

--- src/drivers/ring-generic-chime.groovy
+++ src/drivers/ring-generic-chime.groovy
@@ -15,13 +15,15 @@
  *
  *  Change Log:
  *  2019-03-02: Initial
+ *  2019-11-15: Import URL
  *
  */
 
 import groovy.json.JsonSlurper
 
 metadata {
-  definition(name: "Ring Generic Chime", namespace: "codahq-hubitat", author: "Ben Rimmasch") {
+  definition(name: "Ring Generic Chime", namespace: "codahq-hubitat", author: "Ben Rimmasch",
+    importUrl: "https://raw.githubusercontent.com/codahq/ring_hubitat_codahq/master/src/drivers/ring-generic-chime.groovy") {
     capability "Actuator"
     capability "Tone"
     capability "AudioNotification"
