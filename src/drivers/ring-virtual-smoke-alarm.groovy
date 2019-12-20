e67d81de0942b694f044cade80279eedf8850634
Ben Rimmasch <ben@regoconsulting.com>
2019-12-20 02:01:31 -0700

    + initial

---
+++ src/drivers/ring-virtual-smoke-alarm.groovy
@@ -0,0 +1,104 @@
+/**
+ *  Ring Virtual Smoke Alarm
+ *
+ *  Copyright 2019 Ben Rimmasch
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *  Change Log:
+ *  2019-12-20: Initial
+ *
+ */
+
+import groovy.json.JsonSlurper
+
+metadata {
+  definition(name: "Ring Virtual Smoke Alarm", namespace: "codahq-hubitat", author: "Ben Rimmasch",
+    importUrl: "https://raw.githubusercontent.com/codahq/ring_hubitat_codahq/master/src/drivers/ring-virtual-smoke-alarm.groovy") {
+    capability "Refresh"
+    capability "Sensor"
+    capability "Battery"
+    capability "TamperAlert"
+    capability "SmokeDetector" //smoke - ENUM ["clear", "tested", "detected"]
+
+  }
+
+  preferences {
+    input name: "descriptionTextEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: false
+    input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+    input name: "traceLogEnable", type: "bool", title: "Enable trace logging", defaultValue: false
+  }
+}
+
+private logInfo(msg) {
+  if (descriptionTextEnable) log.info msg
+}
+
+def logDebug(msg) {
+  if (logEnable) log.debug msg
+}
+
+def logTrace(msg) {
+  if (traceLogEnable) log.trace msg
+}
+
+def refresh() {
+  logDebug "Attempting to refresh."
+  //parent.simpleRequest("refresh-device", [dni: device.deviceNetworkId])
+}
+
+def setValues(deviceInfo) {
+  logDebug "updateDevice(deviceInfo)"
+  logTrace "deviceInfo: ${deviceInfo}"
+
+  if (deviceInfo.state && deviceInfo.state.smoke != null) {
+    def smoke = (deviceInfo.state.smoke.alarmStatus == "active") ? "detected" : (deviceInfo.state.smoke.alarmStatus == "inactive" ? "clear" : "tested")
+    checkChanged("smoke", smoke)
+    if (deviceInfo.state.smoke.enabledTimeMs)
+      state.smokeEnabled = deviceInfo.state.smoke.enabledTimeMs
+  }
+  if (deviceInfo.batteryLevel) {
+    checkChanged("battery", deviceInfo.batteryLevel)
+  }
+  if (deviceInfo.tamperStatus) {
+    def tamper = deviceInfo.tamperStatus == "tamper" ? "detected" : "clear"
+    checkChanged("tamper", tamper)
+  }
+  if (deviceInfo.lastUpdate) {
+    state.lastUpdate = deviceInfo.lastUpdate
+  }
+  if (deviceInfo.impulseType) {
+    state.impulseType = deviceInfo.impulseType
+  }
+  if (deviceInfo.lastCommTime) {
+    state.signalStrength = deviceInfo.lastCommTime
+  }
+  if (deviceInfo.nextExpectedWakeup) {
+    state.nextExpectedWakeup = deviceInfo.nextExpectedWakeup
+  }
+  if (deviceInfo.signalStrength) {
+    state.signalStrength = deviceInfo.signalStrength
+  }
+  if (deviceInfo.firmware && device.getDataValue("firmware") != deviceInfo.firmware) {
+    device.updateDataValue("firmware", deviceInfo.firmware)
+  }
+  if (deviceInfo.hardwareVersion && device.getDataValue("hardwareVersion") != deviceInfo.hardwareVersion) {
+    device.updateDataValue("hardwareVersion", deviceInfo.hardwareVersion)
+  }
+
+}
+
+def checkChanged(attribute, newStatus) {
+  if (device.currentValue(attribute) != newStatus) {
+    logInfo "${attribute.capitalize()} for device ${device.label} is ${newStatus}"
+    sendEvent(name: attribute, value: newStatus)
+  }
+}
