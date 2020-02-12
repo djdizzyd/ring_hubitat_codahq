5b2192e4b1f337d02e546e58f935ee83eb0a2a22
Ben Rimmasch <ben@regoconsulting.com>
2020-02-11 17:33:51 -0700

    + initial

---
+++ src/drivers/ring-virtual-flood-freeze-sensor.groovy
@@ -0,0 +1,113 @@
+/**
+ *  Ring Virtual Alarm Flood & Freeze Sensor Driver
+ *
+ *  Copyright 2020 Ben Rimmasch
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *  Change Log:
+ *  2020-02-11: Initial
+ *
+ */
+
+import groovy.json.JsonSlurper
+
+metadata {
+  definition(name: "Ring Virtual Alarm Flood & Freeze Sensor", namespace: "codahq-hubitat", author: "Ben Rimmasch",
+    importUrl: "https://raw.githubusercontent.com/codahq/ring_hubitat_codahq/master/src/drivers/ring-virtual-flood-freeze-sensor.groovy") {
+    capability "Refresh"
+    capability "Sensor"
+    capability "WaterSensor"
+    capability "Battery"
+    capability "TamperAlert"
+
+    attribute "freeze", "string"
+  }
+
+  preferences {
+    input name: "descriptionTextEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: false
+    input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+    input name: "traceLogEnable", type: "bool", title: "Enable trace logging", defaultValue: false
+  }
+}
+
+private logInfo(msg) {
+  if (descriptionTextEnable) log.info msg
+}
+
+def logDebug(msg) {
+  if (logEnable) log.debug msg
+}
+
+def logTrace(msg) {
+  if (traceLogEnable) log.trace msg
+}
+
+def refresh() {
+  logDebug "Attempting to refresh."
+  //parent.simpleRequest("refresh-device", [dni: device.deviceNetworkId])
+}
+
+def setValues(deviceInfo) {
+  logDebug "updateDevice(deviceInfo)"
+  logTrace "deviceInfo: ${deviceInfo}"
+
+  if (deviceInfo.state && deviceInfo.state.faulted != null) {
+    if (deviceInfo.state.flood.faulted != null) {
+      def water = deviceInfo.state.flood.faulted ? "wet" : "dry"
+      checkChanged("water", water)
+    }
+    if (deviceInfo.state.freeze.faulted != null) {
+      def freeze = deviceInfo.state.freeze.faulted ? "detected" : "clear"
+      checkChanged("freeze", freeze)
+    }
+  }
+  if (deviceInfo.batteryLevel) {
+    checkChanged("battery", deviceInfo.batteryLevel, "%")
+  }
+  if (deviceInfo.tamperStatus) {
+    def tamper = deviceInfo.tamperStatus == "tamper" ? "detected" : "clear"
+    checkChanged("tamper", tamper)
+  }
+  if (deviceInfo.lastUpdate) {
+    state.lastUpdate = deviceInfo.lastUpdate
+  }
+  if (deviceInfo.impulseType) {
+    state.impulseType = deviceInfo.impulseType
+  }
+  if (deviceInfo.lastCommTime) {
+    state.signalStrength = deviceInfo.lastCommTime
+  }
+  if (deviceInfo.nextExpectedWakeup) {
+    state.nextExpectedWakeup = deviceInfo.nextExpectedWakeup
+  }
+  if (deviceInfo.signalStrength) {
+    state.signalStrength = deviceInfo.signalStrength
+  }
+  if (deviceInfo.firmware && device.getDataValue("firmware") != deviceInfo.firmware) {
+    device.updateDataValue("firmware", deviceInfo.firmware)
+  }
+  if (deviceInfo.hardwareVersion && device.getDataValue("hardwareVersion") != deviceInfo.hardwareVersion) {
+    device.updateDataValue("hardwareVersion", deviceInfo.hardwareVersion)
+  }
+
+}
+
+def checkChanged(attribute, newStatus) {
+  checkChanged(attribute, newStatus, null)
+}
+
+def checkChanged(attribute, newStatus, unit) {
+  if (device.currentValue(attribute) != newStatus) {
+    logInfo "${attribute.capitalize()} for device ${device.label} is ${newStatus}"
+    sendEvent(name: attribute, value: newStatus, unit: unit)
+  }
+}
